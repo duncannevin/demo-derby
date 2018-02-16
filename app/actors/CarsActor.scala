7c9e14eb7992fdc4ff7a7d069e80876c95eb8f0d
Duncan Nevin <duncan.nevin@gmail.com>
2018-02-16 08:26:26 -0500

    includes cars on response with bad request in order to maintain state. close ws if invalid json

--- app/actors/CarsActor.scala
+++ app/actors/CarsActor.scala
@@ -9,22 +9,35 @@ import play.api.libs.functional.syntax._
 @Named("CarsActor")
 class CarsActor(out: ActorRef)() extends Actor with CarsModel {
   def receive: Receive = {
-    case msg: JsValue => out ! msg
+    case msg: JsValue =>
       val possibleSocketRequest = Json.fromJson[InMessage](msg)
       possibleSocketRequest.asOpt foreach {
         request => self ! request
       }
     case socketResponse: SocketResponse =>
-      out ! socketResponse.response
+      out ! Json.toJson(socketResponse.response)
     case inMessage: InMessage =>
       inMessage match {
-        case InMessage("authenticate", None, None, newCar) => self ! SocketResponse(Json.toJson(insertCar(newCar.head.name, newCar.head.color).getOrElse(cars)))
-        case InMessage("cars", None, None, None) => self ! SocketResponse(Json.toJson(getAllCars.getOrElse(List.empty[Car])))
-        case InMessage("update", key, car, None) => self ! SocketResponse(Json.toJson(updateCar(car.head, key.head).getOrElse(List.empty[Car])))
-        case InMessage("delete", None, car, None) => self ! SocketResponse(Json.toJson(deleteCar(car.head).getOrElse(List.empty[Car])))
-        case _ => self ! SocketResponse(JsString("INVALID REQUEST"))
+        case InMessage("authenticate", None, None, newCar) if newCar.isDefined && newCar.head.isInstanceOf[AddCarForm] =>
+          val uCars = insertCar(newCar.head.name, newCar.head.color)
+          val response = ResponseData(success = uCars.isDefined, inMessage.request, uCars.getOrElse(cars))
+          self ! SocketResponse(response)
+        case InMessage("cars", None, None, None) =>
+          val uCars = getAllCars
+          val response = ResponseData(success = uCars.isDefined, inMessage.request, uCars.getOrElse(cars))
+          self ! SocketResponse(response)
+        case InMessage("update", key, car, None) if key.isDefined && car.isDefined && car.head.isInstanceOf[Car] =>
+          val uCars = updateCar(car.head, key.head)
+          val response = ResponseData(success = uCars.isDefined, inMessage.request, uCars.getOrElse(cars))
+          self ! SocketResponse(response)
+        case InMessage("delete", None, car, None) if car.isDefined && car.get.isInstanceOf[Car] =>
+          val uCars = deleteCar(car.head)
+          val response = ResponseData(success = uCars.isDefined, inMessage.request, uCars.getOrElse(cars))
+          self ! SocketResponse(response)
+        case _ =>
+          self ! SocketResponse(ResponseData(success = false, inMessage.request, cars))
       }
-    case _ => self ! SocketResponse(JsString("FAILED TO CONNECT"))
+    case _ => self ! SocketResponse(ResponseData(success = false, "INVALID INPUT", cars))
   }
 }
 
@@ -32,7 +45,26 @@ object CarsActor {
   def props(out: ActorRef) = Props(new CarsActor(out)())
 }
 
-case class SocketResponse(response: JsValue)
+case class SocketResponse(response: ResponseData)
+
+case class ResponseData(success: Boolean, request: String, cars: List[Car])
+
+object ResponseData {
+  val responseDataReads: Reads[ResponseData] = (
+    (JsPath \ "success").read[Boolean] and
+      (JsPath \ "request").read[String] and
+      (JsPath \ "cars").read[List[Car]]
+    ) (ResponseData.apply _)
+
+  val responseDataWrites: Writes[ResponseData] = (
+    (JsPath \ "success").write[Boolean] and
+      (JsPath \ "request").write[String] and
+      (JsPath \ "cars").write[List[Car]]
+    ) (unlift(ResponseData.unapply))
+
+  implicit val responseDataFormat: Format[ResponseData] =
+    Format(responseDataReads, responseDataWrites)
+}
 
 case class InMessage(request: String, key: Option[String], car: Option[Car], newCar: Option[AddCarForm])
 
